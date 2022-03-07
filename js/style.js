887f3ddeb8a87aae984aef453a3096778531a524
Mahabatan <mahabatan62@gmail.com>
2022-03-07 10:55:01 +0700

    -

--- js/style.js
+++ js/style.js
@@ -1,11 +1,20 @@
-window.addEventListener("load", event => {
-  const mobile_menu_button = document.querySelector("#mobile-menu-button");
-  const menu_nav = document.querySelector("#menu-nav");
-  const close_menu = document.querySelector("#close-menu");
+document.addEventListener("DOMContentLoaded", function () {
+  //The first argument are the elements to which the plugin shall be initialized
+  //The second argument has to be at least a empty object or a object with your desired options
+  // const mobile_menu_button = document.querySelector("#mobile-menu-button");
+  // const menu_nav = document.querySelector("#menu-nav");
+  // const close_menu = document.querySelector("#close-menu");
 
-  [mobile_menu_button, close_menu].forEach(button => {
-    button.addEventListener("click", () => {
-      menu_nav.classList.toggle("-translate-x-full");
-    });
-  });
+  // [mobile_menu_button, close_menu].forEach(button => {
+  //   button.addEventListener("click", () => {
+  //     menu_nav.classList.toggle("-translate-x-full");
+  //   });
+  // });
+  // OverlayScrollbars(document.querySelector("body"), {
+  //   className: "os-theme-dark",
+  // });
+
+  var Scrollbar = window.Scrollbar;
+
+  Scrollbar.init(document.querySelector("body"));
 });
